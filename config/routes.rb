93dfe08be5c4024e7d55ebff18453b51ca6fe9cc
Jeffrey King <jdking33@aol.com>
2016-07-16 09:11:24 -0700

    Submit Assignment 3-Add User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resources :users
+  end
 end
